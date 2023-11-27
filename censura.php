de01cd971d099b2ecde76c84bdf22740bbdb07ec
giuliamari04 <giuliamari04@gmail.com>
2023-11-27 15:31:28 +0100

    decorazioni

--- censura.php
+++ censura.php
@@ -9,17 +9,23 @@ $nuovaFrase = str_replace($parolaCensurata,"***",$frasePresa);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title> PHP Badwords</title>
 </head>
-<body>
+<body class="bg-info">
     <header></header>
-    <main>
-        <p>
-            <?php echo $frasePresa ?>
+    <main class="container d-flex justify-content-center flex-column align-items-center align-content-center" style="height:100vh">
+        <div class="bg-primary p-3 rounded-2 ">
+           <p>
+            <h2 class="text-light">La tua frase completa è...</h2>
+            <h3><?php echo $frasePresa ?></h3>
         </p>
         <p>
-            <?php echo $nuovaFrase ?>
-        </p>
+            <h2 class="text-light">La nuova frase con la parola "<?php echo $parolaCensurata ?>" censurata è...</h2>
+            <h3><?php echo $nuovaFrase ?></h3>
+        </p>  
+        </div>
+       
     </main>
     <footer></footer>
 </body>
--- index.php
+++ index.php
@@ -3,19 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title> PHP Badwords</title>
 </head>
-<body>
+<body class="bg-info">
     <header></header>
-    <main>
-        <form action="censura.php" method="GET">
-            <!-- <label for="frase">frase intera</label> -->
-            <input type="text" name="frase">
+    <main class="container d-flex justify-content-center flex-column align-items-center " style="height:100vh">
+    <div class="bg-primary w-50 p-3 rounded-2 ">
+         <form action="censura.php" method="GET">
+            <label for="frase" class="form-control-plaintext text-light">Inserisci la frase intera</label>
+            <input type="text" class="form-control pb-5"  name="frase" placeholder="frase intera" >
 
-            <!-- <label for="parola">parola da censurare</label> -->
-            <input type="text" name="parola">
-            <button type="submit"> invia </button>
+            <label for="parola" class="form-control-plaintext text-light">Inserisci la parola da censurare</label>
+            <input type="text" class="form-control" name="parola"placeholder="parola censurata" >
+            <button type="submit" class="btn btn-light my-3"> invia </button>
         </form>
+    </div>
+       
     </main>
     <footer></footer>
 </body>
